87c763f1b6b0d702b012f97a3da06f96c4386d0e
Bevan Hendricks <bevan@silvertreebrands.com>
2026-06-07 16:03:38 +0200

    config: update Apps Script URL to latest deployment

--- js/sheets-client.js
+++ js/sheets-client.js
@@ -1,5 +1,5 @@
 // Replace with your deployed Google Apps Script web app URL
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyxXcLPZXb16IXxlo7HRo4AwNjG5kHp-byRjBaHLMEd6CIBisxTqqvhPqMm1KiDgIrY/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbytGT_BjDvOcpPDY8pXo1ETElnM03G0C6Gyh9bZHWBnEFnUXb7KPFgDX4LBMMUx7Hg8/exec';
 
 async function sheetsGet(action, params = {}) {
   const qs = new URLSearchParams({ action, ...params }).toString();
